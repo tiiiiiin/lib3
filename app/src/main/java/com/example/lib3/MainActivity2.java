9a43409df877ea9faa0d2c64743813b7ad888908
Варвара <rrr@outlook.com>
2024-02-22 22:16:19 +0300

    cделан переход между 1, 2 и страницей для книг

--- app/src/main/java/com/example/lib3/MainActivity2.java
+++ app/src/main/java/com/example/lib3/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.lib3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void book(View view) {
+        Intent intent = new Intent(this, addBook.class);
+        startActivity(intent);
     }
 }
