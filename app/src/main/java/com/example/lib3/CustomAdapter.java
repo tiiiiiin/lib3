ed2a3676261b0a990a998ef1fad8a0573f58a474
Варвара <rrr@outlook.com>
2024-02-23 14:16:29 +0300

    display data in recyclerView

---
+++ app/src/main/java/com/example/lib3/CustomAdapter.java
@@ -0,0 +1,61 @@
+package com.example.lib3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+    private Context context;
+    private ArrayList book_id, book_title, book_author, book_count;
+    CustomAdapter(Context context,
+                  ArrayList book_id,
+                  ArrayList book_title,
+                  ArrayList book_author,
+                  ArrayList book_count){
+        this.context = context;
+        this.book_id = book_id;
+        this.book_title = book_title;
+        this.book_author = book_author;
+        this.book_count = book_count;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.book_id_txt.setText(String.valueOf(book_id.get(position)));
+        holder.book_title_txt.setText(String.valueOf(book_title.get(position)));
+        holder.book_author_txt.setText(String.valueOf(book_author.get(position)));
+        holder.book_count_txt.setText(String.valueOf(book_count.get(position)));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return book_id.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView book_id_txt, book_title_txt, book_author_txt, book_count_txt;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            book_id_txt = itemView.findViewById(R.id.book_id_txt);
+            book_title_txt = itemView.findViewById(R.id.book_title_txt);
+            book_author_txt = itemView.findViewById(R.id.book_author_txt);
+            book_count_txt = itemView.findViewById(R.id.book_count_txt);
+        }
+    }
+}
--- app/src/main/java/com/example/lib3/addBook.java
+++ app/src/main/java/com/example/lib3/addBook.java
@@ -1,17 +1,25 @@
 package com.example.lib3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class addBook extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton add_button;
+    MyDatabaseHelper myDB;
+    ArrayList<String> book_id, book_title, book_author, book_count;
+    CustomAdapter customAdapter;
 
 
     @Override
@@ -29,6 +37,33 @@ public class addBook extends AppCompatActivity {
             }
         });
 
+        myDB  = new MyDatabaseHelper(addBook.this);
+        book_id = new ArrayList<>();
+        book_title = new ArrayList<>();
+        book_author = new ArrayList<>();
+        book_count = new ArrayList<>();
 
+
+        storeDataInArrays();
+
+        customAdapter = new CustomAdapter(addBook.this, book_id, book_title, book_author, book_count);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(addBook.this));
+
+    }
+
+    void storeDataInArrays(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show();
+        }else {
+            while(cursor.moveToNext()){
+                book_id.add(cursor.getString(0));
+                book_title.add(cursor.getString(1));
+                book_author.add(cursor.getString(2));
+                book_count.add(cursor.getString(3));
+
+            }
+        }
     }
 }
