e51cd70320a84cb1a16a3592dcfa6d37d5b79ce1
Варвара <rrr@outlook.com>
2024-05-02 17:14:24 +0300

    войти теперь можно только при вводе специального слова

--- app/src/main/java/com/example/lib3/MainActivity.java
+++ app/src/main/java/com/example/lib3/MainActivity.java
@@ -5,16 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText user_field;
+    private Button trans_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void trans_btn(View view) {
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
+        user_field = findViewById(R.id.user_field);
+        trans_btn = findViewById(R.id.trans_btn);
+
+        trans_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = user_field.getText().toString().trim();
+                String requiredWord = "qwerty";
+
+                if (userInput.equals(requiredWord)) {
+
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Неправильное слово", Toast.LENGTH_SHORT).show();
+                    user_field.setText(""); // Очищаем поле ввода
+                }
+            }
+        });
     }
 }
